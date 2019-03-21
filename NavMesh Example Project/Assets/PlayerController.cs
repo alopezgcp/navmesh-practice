d2639e5d30e8cd3c2ca9391f1b004c982517f714
Anthony Lopez <anthony.ron.lopez@gmail.com>
2019-03-20 21:35:35 -0500

    Complete first tutorial video

---
+++ NavMesh Example Project/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera maincam;
+    public NavMeshAgent agent;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = maincam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
