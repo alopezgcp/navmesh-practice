4f31c0e5cbee30092aefbfd6295a320bab4919b7
Anthony Lopez <anthony.ron.lopez@gmail.com>
2019-03-20 22:31:19 -0500

    Complete tutorial

--- NavMesh Example Project/Assets/PlayerController.cs
+++ NavMesh Example Project/Assets/PlayerController.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class PlayerController : MonoBehaviour
 {
     private Camera maincam;
     public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
 
     private void Start()
     {
-        maincam = Camera.main;    
+        maincam = Camera.main;
+        agent.updateRotation = false;
     }
+
     void Update()
     {
         if (Input.GetMouseButton(0))
@@ -21,6 +25,15 @@ public class PlayerController : MonoBehaviour
             {
                 agent.SetDestination(hit.point);
             }
+        }
+
+        if(agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, true, false);
         }
     }
 }
